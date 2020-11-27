e8fa2a27974b0b00664c40ab6579e3d5277f310a
ShazSeatter <shazseatter12@gmail.com>
2020-11-27 11:18:53 +0000

    fixed footer error

--- src/Footer.js
+++ src/Footer.js
@@ -1,12 +1,10 @@
 import React from "react"; 
 import "./Footer.css"; 
 
-export default function() {
+export default function Footer() {
     return (
         <div className="Footer">
             <a href="https://github.com/ShazSeatter/weather-app-react" className="git-hub-respo">Open source code</a> by Shaz Seatter
-
-
         </div>
-    )
+    ); 
 }
