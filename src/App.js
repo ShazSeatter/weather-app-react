c4615e2b5cfa2375760b013f4315aab2c0074fef
ShazSeatter <shazseatter12@gmail.com>
2020-11-30 11:51:24 +0000

    moved footer compone to main app.js

--- src/App.js
+++ src/App.js
@@ -2,17 +2,24 @@ import "./App.css";
 import Header from "./Header";
 import Cities from "./Cities";
 import Content from "./Content";
-import Footer from "./Footer";
 
 function App() {
   return (
     <div className="App">
-        <Header />
-        <Cities />
-        <Content />
-        <Footer />
+      <Header />
+      <Cities />
+      <Content />
+      <div className="Footer">
+        <a
+          href="https://github.com/ShazSeatter/weather-app-react"
+          target="_blank"
+          className="git-hub-respo"
+        >
+          Open source code
+        </a>{" "}
+        by Shaz Seatter
+      </div>
     </div>
   );
 }
-
 export default App;
