af64311ef171259b6e977e991856ec65a85ca63e
ShazSeatter <shazseatter12@gmail.com>
2020-12-07 10:57:30 +0000

    fixed error

--- src/FormattedDate.js
+++ src/FormattedDate.js
@@ -3,7 +3,6 @@ import React from "react";
 export default function FormattedDate(props) {
     let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
     let day = days[props.date.getDay()];
-    let date = props.date.getDate();
     let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
     let month = months[props.date.getMonth()]; 
     let hours = props.date.getHours(); 
